0db0e41870c75a6a00250b62e930c720e67ffcfa
unknown <g_ievenh@MININT-0C18U8G.icc.co.il>
2017-04-23 16:48:56 +0300

    Added object assign pollyfill

--- src/carolina.js
+++ src/carolina.js
@@ -47,7 +47,34 @@
         // API Event functions.
         ON_RESULT = 'ON_RESULT',
 
-        emptyFn = function () { };
+        emptyFn = function () { },
+
+        objectAssignPollyFill = function () {
+            if (typeof Object.assign != 'function') {
+                Object.assign = function (target, varArgs) { // .length of function is 2
+                    'use strict';
+                    if (target == null) { // TypeError if undefined or null
+                        throw new TypeError('Cannot convert undefined or null to object');
+                    }
+
+                    var to = Object(target);
+
+                    for (var index = 1; index < arguments.length; index++) {
+                        var nextSource = arguments[index];
+
+                        if (nextSource != null) { // Skip over if undefined or null
+                            for (var nextKey in nextSource) {
+                                // Avoid bugs when hasOwnProperty is shadowed
+                                if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                                    to[nextKey] = nextSource[nextKey];
+                                }
+                            }
+                        }
+                    }
+                    return to;
+                };
+            }
+        }
 
 
     function Lib() {
@@ -837,10 +864,15 @@
          * @function
          */
         init: function (options) {
-            lib = Lib.create();
             profiler = profilerHandler.create().initialize({
                 logLevel: options.logLevel
             }).setFunctionName('init')
+
+            // Set all pollyfill functions.
+            objectAssignPollyFill();
+
+            // Create the main library instance.
+            lib = Lib.create();
 
             // Start the examination profiler runtime.
             profiler.start();
